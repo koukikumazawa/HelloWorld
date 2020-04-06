275efd96b4eb4a5e035ad3bebfaaf9df9c8aac8b
熊澤 孝紀 <kouki-kumazawa@united.jp>
2020-04-06 09:39:15 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 熊澤 孝紀 on 2020/04/06.
+//  Copyright © 2020 kouki.kumazawa2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
